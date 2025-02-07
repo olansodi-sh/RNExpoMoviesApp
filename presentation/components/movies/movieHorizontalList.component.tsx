9156bfdfc36943ae790723ca4fe276194c6b3933
DevWhoCares <oliversolano2001@gmail.com>
2025-02-06 23:49:19 -0500

    se determina el fin del scroll en la flatlist

--- presentation/components/movies/movieHorizontalList.component.tsx
+++ presentation/components/movies/movieHorizontalList.component.tsx
@@ -1,6 +1,6 @@
 //React
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, NativeSyntheticEvent, NativeScrollEvent } from 'react-native'
+import React, { useRef } from 'react'
 
 //Interfaces
 import { Movie } from '@/infrastructure/interfaces/movie.interface'
@@ -11,9 +11,29 @@ import MoviePosterComponent from './moviePoster.component'
 interface MovieHorizontalListComponentProps {
   movies:Movie[];
   title?:string;
+
+  loadNextPage?:() => void;
 }
 
-const MovieHorizontalListComponent = ({ movies, title }:MovieHorizontalListComponentProps) => {
+const MovieHorizontalListComponent = ({ movies, title, loadNextPage }:MovieHorizontalListComponentProps) => {
+
+  const isLoading = useRef(false)
+
+  const onScroll =  (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if(isLoading.current) return;
+
+    const { contentOffset, contentSize, layoutMeasurement} = event.nativeEvent
+
+    const isEndReached = (contentOffset.x + layoutMeasurement.width + 600) >= contentSize.width ? true : false
+
+    if (!isEndReached) return
+
+      isLoading.current = true
+      console.log('Cargar siguientes peliculas')
+
+      loadNextPage && loadNextPage()
+  }
+
   return (
     <View>
       {
@@ -25,6 +45,7 @@ const MovieHorizontalListComponent = ({ movies, title }:MovieHorizontalListCompo
       showsHorizontalScrollIndicator={false}
       data={movies}
       keyExtractor={(item) => item.id.toString()}
+      onScroll={(event) => onScroll(event)}
       renderItem={({ item }) => (
         <MoviePosterComponent id={item.id} poster={item.poster} smallPoster/>
       )}
